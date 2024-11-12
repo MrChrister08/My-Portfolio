40a8b49ee65f4254e49773f7c5342d085da509d3
MrChrister08 <git@stackblitz.com>
2024-11-12 17:43:54 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
+import { Home } from './pages/Home';
+import { Experience } from './pages/Experience';
+import { Contact } from './pages/Contact';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
+        <Navbar />
+        <main className="container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
